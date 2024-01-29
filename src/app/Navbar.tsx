962a77241ed0ec1f0e41a1929d36aabea0622e42
Kat <katpeace@protonmail.com>
2024-01-28 19:10:00 -0500

    tasks cards designed

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,28 +3,28 @@ import ThemeToggle from "@/context/ThemeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="p-4 flex items-center justify-between uppercase font-semibold dark:bg-perse-100 bg-parchment-200">
-      <div className="flex-1"></div>
-      <ul className="flex justify-center flex-auto">
-        <li className="mx-8">
+    <nav className="p-4 flex items-center justify-between uppercase sm:text-xl dark:bg-perse-100 bg-parchment-200 gap-4">
+      <div className="flex-1 hidden sm:block"></div>
+      <ul className="flex justify-between flex-auto">
+        <li>
           <Link href="/">Home</Link>
         </li>
-        <li className="mx-8">
+        <li>
           <Link href="/tasks">Tasks</Link>
         </li>
-        <li className="mx-8">
+        <li>
           <Link href="/praxis">Praxis</Link>
         </li>
-        <li className="mx-8">
+        <li>
           <Link href="/groups">Groups</Link>
         </li>
       </ul>
-      <ul className="flex justify-end flex-1">
-        <li className="mx-2 cursor-pointer" role="button">
+      <ul className="flex justify-end flex-1 space-between sm:gap-2">
+        <li className=" cursor-pointer" role="button">
           <Link href="/more">❔</Link>
         </li>
         <ThemeToggle />
-        <li className="mx-2">👤</li>
+        <li>👤</li>
       </ul>
     </nav>
   );
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 import dummyTasks from "@/api/dummyTasks";
 import { Task } from "@/types";
 import {
@@ -6,7 +8,6 @@ import {
   MagnifyingGlassIcon,
   XMarkIcon,
 } from "@heroicons/react/24/solid";
-import Link from "next/link";
 
 type CardProps = {
   task: Task;
@@ -16,46 +17,78 @@ const Card = ({ task }: CardProps) => {
   return (
     <Link
       href={`/tasks/${task.id}`}
-      className="w-full mx-auto p-4 flex flex-col justify-between leading-normal cursor-pointer rounded-lg border border-transparent dark:text-parchment-100 bg-white dark:bg-perse-100 hover:border-gray-400 dark:border-perse-50 dark:hover:bg-perse-400"
+      className="w-full p-4 flex justify-between gap-4 cursor-pointer rounded-lg border border-transparent dark:text-parchment-100 bg-white dark:bg-perse-100 hover:border-gray-400 dark:border-perse-50 dark:hover:bg-perse-400"
     >
-      {/* Name & Status */}
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-lg font-bold">{task.name}</h2>
-        <span
-          className={`rounded ${
-            task.status === "active"
-              ? "dark:bg-lime-920 bg-green-300"
-              : task.status === "retired"
-              ? "dark:bg-gray-900 bg-gray-400"
-              : "dark:bg-gray-700 bg-gray-200"
-          } dark:text-parchment-100 px-2 py-1 text-xs font-semibold uppercase`}
-        >
-          {task.status}
-        </span>
+      {/* Image */}
+      <div className="mx-auto max-w-48 hidden md:block">
+        <Image
+          src={task.image_path || ""}
+          alt={task.name}
+          layout="responsive"
+          width={16}
+          height={24}
+          objectFit="cover"
+          className="rounded"
+        />
       </div>
-      <div className="flex justify-between items-center mb-4">
-        {/* Creator & Date */}
-        <div className="flex justify-between items-center">
-          <span className="font-bold mr-2 text-sm">by {task.creator}</span>
-          <span>{task.created_at}</span>
-        </div>
-        {/* Icons */}
-        <div className="flex justify-between items-center gap-4  w-1/4">
-          <span className="font-bold px-1 dark:bg-perse-100 dark:text-parchment-300">
-            {task.points}P
-          </span>
-          <div className="flex items-center justify-center rounded-full border dark:border-parchment-300 dark:bg-parchment-300 dark:text-perse-100 px-2">
-            <span className="font-bold text-sm">{task.level}</span>
+      <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col">
+          <div className="w-full flex justify-between items-center mb-4">
+            <div className="w-full flex items-center gap-2">
+              {/* Image */}
+              <div className="max-w-12 block md:hidden">
+                <Image
+                  src={task.image_path || ""}
+                  alt={task.name}
+                  layout="responsive"
+                  width={16}
+                  height={24}
+                  objectFit="cover"
+                  className="rounded"
+                />
+              </div>
+              {/* Name & Status */}
+              <h2 className="text-lg font-bold">{task.name}</h2>
+            </div>
+            <span
+              className={`rounded ${
+                task.status === "active"
+                  ? "dark:bg-lime-920 bg-green-300"
+                  : task.status === "retired"
+                  ? "dark:bg-gray-900 bg-gray-400"
+                  : "dark:bg-gray-700 bg-gray-200"
+              } dark:text-parchment-100 px-2 py-1 text-xs font-semibold uppercase`}
+            >
+              {task.status}
+            </span>
           </div>
-          <AcademicCapIcon className="h-7 w-7 text-gray-500 dark:text-parchment-300" />
-          <span className="font-bold">{task.participants}人</span>
+          <div className="flex flex-col lg:flex-row justify-between items-start sm:items-center mb-4">
+            {/* Creator & Date */}
+            <div className="w-full flex items-center justify-between lg:justify-start mb-4 lg:mb-0">
+              <span className="font-bold mr-2 text-sm">by {task.creator}</span>
+              <span>{task.created_at}</span>
+            </div>
+            {/* Icons */}
+            <div className="flex justify-between items-center w-full lg:w-1/2">
+              <span className="font-bold text-gray-500  dark:bg-perse-100 dark:text-parchment-300">
+                {task.points}P
+              </span>
+              <div className="px-2 flex items-center justify-center rounded-full border dark:border-parchment-300 dark:bg-parchment-300 dark:text-perse-100  font-bold text-sm text-white bg-gray-500">
+                <span className="">{task.level}</span>
+              </div>
+              <AcademicCapIcon className="h-7 w-7 text-gray-500 dark:text-parchment-300" />
+              <span className="font-bold text-gray-500 dark:text-parchment-300">
+                {task.participants}人
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-      {/* Description */}
-      <div className="bg-gray-100 dark:bg-perse-200  rounded-lg p-3 border border-gray-300 dark:border-perse-50">
-        <p className="text-gray-700 dark:text-parchment-100 text-base line-clamp-3">
-          {task.description}
-        </p>
+        {/* Description */}
+        <div className="w-full flex-grow bg-gray-100 dark:bg-perse-200 rounded-lg p-3 border border-gray-300 dark:border-perse-50">
+          <p className="text-gray-700 dark:text-parchment-100 text-base line-clamp-3">
+            {task.description}
+          </p>
+        </div>
       </div>
     </Link>
   );
@@ -63,7 +96,8 @@ const Card = ({ task }: CardProps) => {
 
 export default function TasksPage() {
   return (
-    <main className="flex flex-col items-center my-24 mx-3 md:mx-56">
+    <main className="flex flex-col items-center my-24 mx-6 md:mx-24 xl:mx-64">
+      {/* Header + Search/Filter/Sort */}
       <section className="flex w-full justify-between items-center mb-4 gap-4">
         <h1 className="font-bold dark:text-gray-400">Tasks</h1>
         <div className="w-full relative">
@@ -83,6 +117,7 @@ export default function TasksPage() {
           </div>
         </div>
       </section>
+      {/* Task Cards */}
       <section className="w-full flex flex-col items-center gap-6">
         {dummyTasks.map((task) => (
           <Card key={task.id} task={task} />
